ecdaef400dad02674fa0f6185789d770a5b3ca4d
Gourisankar Das <gourisankar.das@broadcom.com>
2020-02-16 09:10:38 -0800

    Added uptime from local repo - Sun Feb 16 09:10:38 PST 2020 - lvnvdb1072.lvn.broadcom.net

--- random_no.sh
+++ random_no.sh
@@ -12,4 +12,5 @@ s1000=`$SHUF -i 1-1000 | awk 'BEGIN {s=0} {s += $NF} END {print s}'`
 echo "++ First 10 sum: $s10"
 echo "++ First 100 sum: $s100"
 echo "++ First 1000 sum: $s1000"
+uptime
 # all done
