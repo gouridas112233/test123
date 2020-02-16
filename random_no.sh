1788a22e2986a73d1a7c6ab05018b2c9a751a9d6
Gourisankar Das <gourisankar.das@broadcom.com>
2020-02-15 19:20:52 -0800

    Add random.sh file in dev_branch_1

---
+++ random_no.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Generate random numbers and add them
+#------------------------------------------
+prog=$0
+SHUF=/usr/bin/shuf
+
+echo "=== Generate random numbers and add them"
+s10=`$SHUF -i 1-10|awk 'BEGIN {s=0} {s += $NF} END {print s}'`
+s100=`$SHUF -i 1-100 | awk 'BEGIN {s=0} {s += $NF} END {print s}'`
+s1000=`$SHUF -i 1-1000 | awk 'BEGIN {s=0} {s += $NF} END {print s}'`
+echo "++ First 10 sum: $s10"
+echo "++ First 100 sum: $s100"
+echo "++ First 1000 sum: $s1000"
